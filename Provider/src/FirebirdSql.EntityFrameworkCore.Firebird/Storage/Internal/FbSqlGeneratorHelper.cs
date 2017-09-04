c43fb81452d32bc083a864c4929e49d72c381be1
ralmsdeveloper <ralms@ralms.net>
2017-09-04 14:42:10 -0300

    Unnecessary code removed

--- Provider/src/FirebirdSql.EntityFrameworkCore.Firebird/Storage/Internal/FbSqlGeneratorHelper.cs
+++ Provider/src/FirebirdSql.EntityFrameworkCore.Firebird/Storage/Internal/FbSqlGeneratorHelper.cs
@@ -22,128 +22,94 @@
  *                  All Rights Reserved.
  */
 
-using System;
-using System.Globalization;
 using System.Text;
 using JetBrains.Annotations;
-using Microsoft.EntityFrameworkCore.Utilities;
-using Microsoft.EntityFrameworkCore.Update;
-using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
-using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Utilities; 
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal; 
 
 namespace Microsoft.EntityFrameworkCore.Storage.Internal
 {
-    public class FbSqlGenerationHelper : RelationalSqlGenerationHelper
-    {
-        internal readonly IFbOptions _options = null;
-        public FbSqlGenerationHelper([NotNull] RelationalSqlGenerationHelperDependencies dependencies,
-              [NotNull] IFbOptions options)
-            : base(dependencies)
-        {
-            _options = options;
-        }
+	public class FbSqlGenerationHelper : RelationalSqlGenerationHelper
+	{
+		private readonly IFbOptions _options;
 
-        /// <summary>
-        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public override string EscapeIdentifier(string identifier)
-            => Check.NotEmpty(identifier, nameof(identifier));
+		public FbSqlGenerationHelper([NotNull] RelationalSqlGenerationHelperDependencies dependencies,
+			[NotNull] IFbOptions options)
+			: base(dependencies)
+		{
+			_options = options;
+		}
 
-        /// <summary>
-        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public override void EscapeIdentifier(StringBuilder builder, string identifier)
-        {
-            Check.NotEmpty(identifier, nameof(identifier));
+		/// <summary>
+		///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		public override string EscapeIdentifier(string identifier)
+		{
+			return Check.NotEmpty(identifier, nameof(identifier));
+		}
 
-            var initialLength = builder.Length;
-            builder.Append(identifier.MaxLength(_options.ConnectionSettings.ServerVersion.ObjectLengthName));
-        }
+		/// <summary>
+		///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		public override void EscapeIdentifier(StringBuilder builder, string identifier)
+		{
+			Check.NotEmpty(identifier, nameof(identifier));
+			builder.Append(identifier.MaxLength(_options.ConnectionSettings.ServerVersion.ObjectLengthName));
+		}
 
-        /// <summary>
-        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public override string DelimitIdentifier(string identifier)
-            => $"\"{EscapeIdentifier(Check.NotEmpty(identifier.MaxLength(_options.ConnectionSettings.ServerVersion.ObjectLengthName), nameof(identifier)))}\""; // Interpolation okay; strings
+		/// <summary>
+		///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		public override string DelimitIdentifier(string identifier)
+		{
+			return
+				$"\"{EscapeIdentifier(Check.NotEmpty(identifier.MaxLength(_options.ConnectionSettings.ServerVersion.ObjectLengthName), nameof(identifier)))}\"";
+		}
 
-        /// <summary>
-        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public override void DelimitIdentifier(StringBuilder builder, string identifier)
-        {
-            Check.NotEmpty(identifier, nameof(identifier));
-            builder.Append('"');
-            EscapeIdentifier(builder, identifier.MaxLength(_options.ConnectionSettings.ServerVersion.ObjectLengthName));
-            builder.Append('"');
-        }
+		/// <summary>
+		///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		public override void DelimitIdentifier(StringBuilder builder, string identifier)
+		{
+			Check.NotEmpty(identifier, nameof(identifier));
+			builder.Append('"');
+			EscapeIdentifier(builder, identifier.MaxLength(_options.ConnectionSettings.ServerVersion.ObjectLengthName));
+			builder.Append('"');
+		}
 
-        //
-        // Summary:
-        //     Generates a valid parameter name for the given candidate name.
-        //
-        // Parameters:
-        //   name:
-        //     The candidate name for the parameter.
-        //
-        // Returns:
-        //     A valid name based on the candidate name.
-        public override string GenerateParameterName(string name)
-        =>   $"@{name.MaxLength(_options.ConnectionSettings.ServerVersion.ObjectLengthName)}";
-        
+		//
+		// Summary:
+		//     Generates a valid parameter name for the given candidate name.
+		//
+		// Parameters:
+		//   name:
+		//     The candidate name for the parameter.
+		//
+		// Returns:
+		//     A valid name based on the candidate name.
+		public override string GenerateParameterName(string name)
+		{
+			return $"@{name.MaxLength(_options.ConnectionSettings.ServerVersion.ObjectLengthName)}";
+		}
 
-        //
-        // Summary:
-        //     Writes a valid parameter name for the given candidate name.
-        //
-        // Parameters:
-        //   builder:
-        //     The System.Text.StringBuilder to write generated string to.
-        //
-        //   name:
-        //     The candidate name for the parameter.
-        public override void GenerateParameterName(StringBuilder builder, string name)
-       => builder.Append("@").Append(name.MaxLength(_options.ConnectionSettings.ServerVersion.ObjectLengthName));
 
-    
-
-        public static object GenerateValue(ColumnModification column)
-        {
-            object value = null;
-            if (column.Property.ClrType == typeof(string))
-                value = ($"'{column.Value}'");
-            else if (column.Property.ClrType == typeof(int)
-                 || column.Property.GetType() == typeof(int?)
-                 || column.Property.GetType() == typeof(long)
-                 || column.Property.GetType() == typeof(long?)
-                 )
-                value = (column.Value);
-            else if (column.Property.ClrType == typeof(decimal)
-               || column.Property.GetType() == typeof(decimal?)
-               || column.Property.GetType() == typeof(double)
-               || column.Property.GetType() == typeof(double?)
-           )
-                value = (column.Value);
-            else if (column.Property.ClrType == typeof(DateTime)
-              || column.Property.GetType() == typeof(DateTime?)
-              || column.Property.GetType() == typeof(TimeSpan)
-              || column.Property.GetType() == typeof(TimeSpan?)
-          )
-                value = ((column.Value == null ? null : $"'{DateTime.Parse(column.Value.ToString()).ToString("yyyy-MM-dd HH:mm:ss.ffff")}'"));
-
-            else
-                value = ($"'{column.Value}'");
-
-            return value;
-        }
-
-        public static void GenerateValue(StringBuilder builder, ColumnModification column)
-        {
-            builder.Append(GenerateValue(column));
-        }
-         
-    }
+		//
+		// Summary:
+		//     Writes a valid parameter name for the given candidate name.
+		//
+		// Parameters:
+		//   builder:
+		//     The System.Text.StringBuilder to write generated string to.
+		//
+		//   name:
+		//     The candidate name for the parameter.
+		public override void GenerateParameterName(StringBuilder builder, string name)
+		{
+			builder.Append("@").Append(name.MaxLength(_options.ConnectionSettings.ServerVersion.ObjectLengthName));
+		}
+	}
 }
